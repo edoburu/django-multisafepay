226bea6d4e42be66aed63d3a2ff85838b5245cc4
Diederik van der Boor <vdboor@edoburu.nl>
2014-02-26 22:45:07 +0100

    Add return_link_text to OrderNotifyView

--- django_multisafepay/views.py
+++ django_multisafepay/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.utils.html import format_html
 from django.views.generic import View
+from django.utils.translation import ugettext_lazy as _
 from django_multisafepay.client import MultiSafepayClient
 from django_multisafepay.signals import order_status_changed
 
@@ -8,6 +10,8 @@ class OrderNotifyView(View):
     """
     View to be called by MultiSafepay when a status update occured.
     """
+    return_link_text = _('Return to webshop')
+
 
     def get(self, request, *args, **kwargs):
         self.transaction_id = request.GET['transactionid']
@@ -23,7 +27,7 @@ class OrderNotifyView(View):
         if self.type == 'initial':
             # displayed at the last page of the transaction proces (if no redirect_url is set)
             url = request.build_absolute_uri('/')
-            return HttpResponse(u'<a href="{0}">Return to shop</a>'.format(url))
+            return HttpResponse(format_html(u'<a href="{0}">{1}</a>', url, self.return_link_text))
         else:
             # MultiSafepay back-end expects an "ok" if no error occurred
             return HttpResponse('ok')

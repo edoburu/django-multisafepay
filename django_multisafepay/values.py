c3a3c578701c1d886dc7951cdbeb13d7ff42d4f0
Diederik van der Boor <vdboor@edoburu.nl>
2018-03-19 15:17:02 +0100

    Remove unused values.py

--- django_multisafepay/values.py
+++
@@ -1,9 +0,0 @@
-"""
-The data objects can be provided as parameters to the client.
-"""
-from decimal import Decimal
-from xml.sax.saxutils import escape
-
-from django.core.urlresolvers import reverse
-from django.utils.encoding import force_text
-from django_multisafepay import PLUGIN_VERSION, SHOP_NAME, appsettings
